697c97f150400dc702c08a9e90b2eb484a965509
admi <nariman.ramazaov@yandex.ru>
2023-04-09 07:48:46 +0300

    add

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,12 +2,16 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([3, 2, 1], -1, 'test') == 1
-    assert arrs.get([1, 2], 2, 'test') == 'test'
-
+    assert arrs.get('str', 1, 'test') == 'Тип данных не является списком'
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], '2', 'test') == 'Индекс указон не корректно'
+    assert arrs.get([1, 2, 3, 4], [1, 2], 'test') == 'Индекс указон не корректно'
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice('asd', 2) == 'Тип данных не является списком'
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
